74ade0e663d92a849113dce0751879fdb43036ee
MGehad <134580414+MGehad@users.noreply.github.com>
2024-02-23 10:06:53 +0200

    Add the seventh problem

---
+++ Task 1/Problem7.dart
@@ -0,0 +1,11 @@
+void main() {
+  double distance = 25;
+  double speed = 40;
+  double time;
+
+  time = distance / speed;
+
+  double timeInMinutes = time * 60;
+
+  print("The time in minutes = $timeInMinutes minutes");
+}
