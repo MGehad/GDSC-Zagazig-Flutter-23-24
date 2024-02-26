68d122f0ed3e2a41410febceaf458f779ee078ba
MGehad <134580414+MGehad@users.noreply.github.com>
2024-02-26 07:18:44 +0200

    Adding the first part

---
+++ Task 2/p1.dart
@@ -0,0 +1,5 @@
+// 1. (a) var myMap = {key1: value1,key2: value2};
+// 2. (d) All of the above.
+// 3. (c) To pass data back to the caller when the function finishes
+// 4. (c) while loop
+// 5. (a) map creates a new List, while forEach modifies the original List.
