c5789451349a6cd7e812e8c46ee688ea02c6443b
MGehad <134580414+MGehad@users.noreply.github.com>
2024-02-29 13:19:26 +0200

    Adding the first part

---
+++ Task 3/p1.dart
@@ -0,0 +1,5 @@
+// 1. B) this
+// 2. D) The subclass method replaces the method in the superclass
+// 3. B) Three: default, named, and factory
+// 4. C) By using the ‘with’ keyword
+// 5. A) Mixins can have constructors
