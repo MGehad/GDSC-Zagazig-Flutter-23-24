25e5f50993416918c24cfc9982d1ae761316fb76
MGehad <134580414+MGehad@users.noreply.github.com>
2024-03-03 19:49:28 +0200

    Adding the first part

---
+++ Task 4/p1.dart
@@ -0,0 +1,5 @@
+//1. a) To hide implementation details and control data access.
+//2. c) Prefacing the property name with an underscore (_).
+//3. a) The ability for objects of different classes to respond to the same method call in different ways.
+//4. b) Method overriding
+//5. d) All of the above.
