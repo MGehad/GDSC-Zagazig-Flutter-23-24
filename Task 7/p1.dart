6f1f6f7ca6da224ebbffe91608c4ee7b4a712d3d
MGehad <134580414+MGehad@users.noreply.github.com>
2024-05-02 18:00:47 +0300

    Added the first part

---
+++ Task 7/p1.dart
@@ -0,0 +1,5 @@
+//1. C) To create a scrollable list of children
+//2. A) To shrink the ListView widget to fit its children's size
+//3. B) To create a fixed-size box.
+//4. B) backgroundImage
+//5. B) backgroundImage
